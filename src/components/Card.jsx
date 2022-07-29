b084571c4e0e075a9b97b4e6aac001598d4a896c
Samuel Christopher <ebusameric@gmail.com>
2022-07-29 12:52:16 +0100

    feature: card product display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
-import Image from './Image';
-import productImage from '../images/product-image.jpg';
+import addToCartIcon from '../assets/add-to-cart.svg';
 import { Link } from 'react-router-dom';
 
+class Card extends React.Component {
+    render(){
+        const {currency, addToCart, product} = this.props;
+        const { id, inStock, gallery, name, prices } = product;
+        const price = prices.find(el => el.currency.label === currency.label);
+        return (
+            <Container>
+                <ProductUrl to={'/product/'+id}>
+                    <ProductImg src={gallery[0]}>
+                        {inStock && <Span>OUT OF STOCK</Span>}
+                        <AddToCartIcon onClick={(e) => addToCart(e, product)} src={addToCartIcon}/>
+                    </ProductImg>
+                    <Title>{name}</Title>
+                    <PriceTag>{price.currency.symbol + price.amount.toFixed(2)}</PriceTag>
+                </ProductUrl>
+                {inStock && <Overlay />}
+            </Container>
+        )
+    }
+}
+
+export default Card;
+
+
+const AddToCartIcon = styled.a`
+    display: none;
+    width: 52px;
+    height: 52px;
+    background-image: url(${props => props.src});
+    background-size: contain;
+    background-repeat: no-repeat;
+    position: absolute;
+    bottom: -20px;
+    right: 20px;
+`;
 const Container = styled.div`
-    padding: 16px;
+    padding: 14px;
     height: 444px;
     margin: 40px auto;
+    &:hover ${AddToCartIcon} {
+        display: flex;
+    }
     &:hover {
         box-shadow: 0px 2px 5px 0px #c7c7c7;
     }
+    position: relative;
 `;
-
 const ProductUrl = styled(Link)`
     text-decoration: none;
 `;
-
 const Title = styled.p`
     font-weight: 300;
     font-size: 18px;
@@ -32,17 +68,33 @@ const PriceTag = styled.span`
     padding: 10px 0;
 `;
 
-function Card ({id, photo, name, price}) {
-    return (
-        <Container>
-            <ProductUrl to={'/product/'+id}>
-                <Image photo={photo} />
-                <Title>{name}</Title>
-                <PriceTag>$50.00</PriceTag>
-            </ProductUrl>
-        </Container>
-    )
-}
+const ProductImg = styled.div`
+    display: block;
+    width: 350px;
+    height: 330px;
+    background-image: url(${props => props.src});
+    background-size: cover;
+    position: relative;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
-export default Card;
+const Overlay = styled.div`
+    background: #fffefe;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    opacity: 0.3;
+`;
 
+const Span = styled.span`
+    font-family: Raleway;
+    font-size: 24px;
+    font-weight: 400;
+    letter-spacing: 0px;
+    color: #8D8F9A;
+`;
