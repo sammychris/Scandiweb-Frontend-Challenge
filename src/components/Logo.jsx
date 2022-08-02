925a95affd6a8363ba6648b510d915f3a3884007
Samuel Christopher <ebusameric@gmail.com>
2022-08-02 12:58:45 +0100

    feature: create a logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../images/logo.svg';
-import {
-    BrowserRouter as Router,
-    Link,
-    useLocation
-  } from "react-router-dom";
+import logo from '../assets/logo.svg';
+import { Link } from "react-router-dom";
 
-
-function Logo ({style}) {
-    const Icon = styled(Link)`
-        display: flex;
-        Width: 31.18px;
-        Height: 28.62px;
-        background: url(${logo});
-    `;
-    return (<Icon to="/" style={style}/>);
+class Logo extends React.Component{
+    render() {
+        const {style} = this.props;
+        return (<Icon to="/" src={logo} style={style}/>);
+    }
 }
 
 export default Logo;
+
+const Icon = styled(Link)`
+    display: flex;
+    Width: 31.18px;
+    Height: 28.62px;
+    background: url(${props => props.src});
+`;
