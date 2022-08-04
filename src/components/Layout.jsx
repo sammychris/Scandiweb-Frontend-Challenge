dd6444b6315e0d74aee165fde93faaf638267132
Samuel Christopher <ebusameric@gmail.com>
2022-08-04 13:02:19 +0100

    feature: add layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Layout ({children}) {
-    const LayoutStyle = styled.div`
-        max-width: 1230px;
-        width: 90%;
-        margin: 80px auto;
-    `;
-    return(
-        <LayoutStyle>
-            {children}
-        </LayoutStyle>
-    )
+class Layout extends React.Component {
+    render() {
+        const { children } = this.props;
+        return(
+            <LayoutStyle>
+                {children}
+            </LayoutStyle>
+        )
+    }
 }
 
 export default Layout;
+
+const LayoutStyle = styled.div`
+    max-width: 1230px;
+    width: 90%;
+    margin: 80px auto;
+`;
