a97c4794971acd197a4ad1fc35f2c2b1ccdf4183
Samuel Christopher <ebusameric@gmail.com>
2022-08-06 13:07:01 +0100

    feature: create reducers for state management

---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const reducerSlice = createSlice({
+    name: 'reducer',
+    initialState: {
+        cart: [],
+        currency: {label: "GBP", symbol: "$"},
+        isCartOverlay: false,
+    },
+    reducers: {
+        setCart: (state, action) => {
+            state.cart = action.payload;
+        },
+        setCurrency: (state, action) => {
+            state.currency = action.payload;
+        },
+        setIsCartOverlay: (state, action) => {
+            state.isCartOverlay = action.payload;
+        },
+    },
+});
+
+export const { setCart, setCurrency, setIsCartOverlay } = reducerSlice.actions;
+
+export default reducerSlice.reducer;
