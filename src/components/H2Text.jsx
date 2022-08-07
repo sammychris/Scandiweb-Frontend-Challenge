dcb2054f7a835df6661d5b9d391865d22ced4794
Samuel Christopher <ebusameric@gmail.com>
2022-08-07 13:12:51 +0100

    add h2 component

--- src/components/H2Text.jsx
+++ src/components/H2Text.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function H2Text ({children, style}) {
-    const Heading = styled.h2`
-        font-weight: 600;
-        font-size: 30px;
-        margin-bottom: 16px;
-    `;
-    return (<Heading style={style}>{children}</Heading>);
+class H2Text extends React.Component {
+    render() {
+        const {children, style} = this.props;
+        return (<Heading style={style}>{children}</Heading>);
+    }
 }
 
 export default H2Text;
+
+
+const Heading = styled.h2`
+    font-weight: 600;
+    font-size: 30px;
+    margin-bottom: 16px;
+`;
