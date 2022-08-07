edf90ab65ef763e057337e284d8a9b1b4c91ea99
Samuel Christopher <ebusameric@gmail.com>
2022-08-07 13:12:40 +0100

    add h1 component

--- src/components/H1Text.jsx
+++ src/components/H1Text.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function H1Text ({children, style}) {
-    const Heading = styled.h1`
-        font-weight: 400;
-        font-size: 42px;
-        text-transform: capitalize;
-    `;
-    return (<Heading style={style}>{children}</Heading>)
+class H1Text extends React.Component {
+    render() {
+        const {children, style} = this.props;
+        return (<Heading style={style}>{children}</Heading>)
+    }
 }
 
 export default H1Text;
+
+const Heading = styled.h1`
+    font-weight: 400;
+    font-size: 42px;
+    text-transform: capitalize;
+`;
