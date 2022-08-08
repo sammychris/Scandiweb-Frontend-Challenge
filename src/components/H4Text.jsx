a008672c585b3de71143e5b51753046da28b8db8
Samuel Christopher <ebusameric@gmail.com>
2022-08-08 13:13:24 +0100

    add h4 component

--- src/components/H4Text.jsx
+++ src/components/H4Text.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function H4Text ({style, children}) {
-    const Heading = styled.h3`
-        font-weight: 700;
-        font-size: 18px;
-        margin-bottom: 8px;
-        text-transform: uppercase;
-    `;
-    return (<Heading style={style}>{children}</Heading>)
+
+class H4Text extends React.Component {
+    render() {
+        const {style, children} = this.props;
+        return (<Heading style={style}>{children}</Heading>)
+    }
 }
 
 export default H4Text;
+
+
+const Heading = styled.h3`
+    font-weight: 700;
+    font-size: 18px;
+    margin-bottom: 8px;
+    text-transform: uppercase;
+`;
