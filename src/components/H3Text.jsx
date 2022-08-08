ffaeb55a8899263954a0c40458703095e00f6d85
Samuel Christopher <ebusameric@gmail.com>
2022-08-08 13:13:11 +0100

    add h3 component

--- src/components/H3Text.jsx
+++ src/components/H3Text.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function H3Text ({style, children}) {
-    const Heading = styled.h3`
-        font-weight: 700;
-        font-size: 24px;
-        margin-bottom: 8px;
-    `;
-    return (<Heading style={style}>{children}</Heading>)
+
+class H3Text extends React.Component{
+    render() {
+        const {style, children} = this.props
+        return (<Heading style={style}>{children}</Heading>)
+    }
 }
 
 export default H3Text;
+
+const Heading = styled.h3`
+    font-weight: 700;
+    font-size: 24px;
+    margin-bottom: 8px;
+`;
