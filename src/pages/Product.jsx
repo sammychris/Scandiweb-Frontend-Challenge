cebb168837d3a210d5816ef2d3033905cedd960e
Samuel Christopher <ebusameric@gmail.com>
2022-08-10 13:21:30 +0100

    feature: complete product page and functionality

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,24 +1,79 @@
 import React from 'react';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
-import Image from '../components/Image';
-import productImage from '../images/product-image.jpg';
-import ProductSizes from '../components/ProductSizes';
-import ProductColors from '../components/ProductColors';
-import Attributes from '../components/Attributes';
+import SizeBox from '../components/SizeBox';
+import ColorBox from '../components/ColorBox';
 import H2Text from '../components/H2Text';
 import H3Text from '../components/H3Text';
 import H4Text from '../components/H4Text';
-import CommonBtn from '../components/CommonBtn';
+import CommonButton from '../components/CommonButton';
 import Paragraph from '../components/Paragraph';
-import { useQuery, gql } from '@apollo/client';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useParams
-} from "react-router-dom";
+import withProduct from '../hoc/withProduct';
+import Loader from '../components/Loader';
+
+
+class Product extends React.Component {
+    render(){
+        const { loading, product, onSelect, currency, addToCart, galleryIndex, setGalleryIndex } = this.props;
+        const price = product?.prices?.find(el => el.currency.label === currency.label);
+        if (loading) return <Loader />
+        return (
+            <Layout>
+                <ProductContainer>
+                    <ProductImages>
+                        <Gallery>
+                            {
+                               product.gallery.map((url, i) =>  <ImgItem key={i} src={url} href="#" onMouseEnter={() => setGalleryIndex(i)} onMouseLeave={() => setGalleryIndex(0)}/>)
+                            }
+                        </Gallery>
+                        <Image src={product.gallery[galleryIndex]} />
+                    </ProductImages>
+                    <ProductDetails>
+                        <H2Text>{product.brand}</H2Text>
+                        <H2Text style={{fontWeight: 400, marginBottom: 43}}>{product.name}</H2Text>
+                        {
+                            product.attributes?.map((attr, i) => (
+                                <div id={attr.id} key={i}>
+                                    <H4Text>{attr.name}: </H4Text>
+                                    <Boxes>
+                                        {
+                                            attr.type === 'swatch'?
+                                            attr.items.map((item, i) => (
+                                                <ColorBox
+                                                    selected={item.selected}
+                                                    value={item.value}
+                                                    id={item.id}
+                                                    key={i}
+                                                    onClick={e => onSelect({e, attr, item})}
+                                                />
+                                            )):
+                                            attr.items.map((item, i) => (
+                                                <SizeBox
+                                                    selected={item.selected}
+                                                    value={item.value} 
+                                                    id={item.id}
+                                                    key={i}
+                                                    onClick={e => onSelect({e, attr, item})}    
+                                                />
+                                            ))
+                                        }  
+                                    </Boxes> 
+                                </div>
+                                )
+                            )
+                        }
+                        <H4Text style={{marginBottom: 20}}>PRICE:</H4Text>
+                        <H3Text style={{marginBottom: 30}}>{price.currency.symbol + price.amount.toFixed(2)}</H3Text>
+                        <CommonButton onClick={addToCart} style={{marginBottom: 40}}>Add to Cart</CommonButton>
+                        <Paragraph>{product.description}</Paragraph>
+                    </ProductDetails>
+                </ProductContainer>
+            </Layout>
+        )
+    }
+}
+
+export default withProduct(Product);
 
 
 const ProductContainer = styled.div`
@@ -37,79 +92,22 @@ const ProductDetails = styled.div`
     width: 292px;
     margin: 0 50px;
 `;
-const SelectItem = ({photo}) => {
-    const Item = styled.a`
-        display: flex;
-        width: 79px;
-        height: 80px;
-        background-image: url(${photo});
-        background-size: cover;
-        margin-bottom: 20px;
-    `;
-    return(
-        <Item href="" />
-    )
-}
-
-function Product () {
-    let { id } = useParams();
-    const { loading, error, data } = useQuery(QUERY(id));
-    if (loading) return <div>Loading...</div>
-    const { name, brand, gallery, description, category, attributes, prices} = data?.product;
-    return (
-        <Layout>
-            <ProductContainer>
-                <ProductImages>
-                    <Gallery>
-                        {
-                            gallery.map(el => <SelectItem photo={el}/>)
-                        }
-                    </Gallery>
-                    <Image photo={gallery[0]} style={{width:610, height:'auto', margin: '0 50px'}}/>
-                </ProductImages>
-                <ProductDetails>
-                    <H2Text>{brand}</H2Text>
-                    <H2Text style={{fontWeight: 400, marginBottom: 43}}>{name}</H2Text>
-                    <Attributes attr={attributes}/>
-                    <H4Text style={{marginBottom: 20}}>PRICE:</H4Text>
-                    <H3Text style={{marginBottom: 30}}>$50.00</H3Text>
-                    <CommonBtn style={{marginBottom: 40}}>Add to Cart</CommonBtn>
-                    <Paragraph>{description}</Paragraph>
-                </ProductDetails>
-            </ProductContainer>
-        </Layout>
-    )
-}
-
-export default Product;
-
-
-const QUERY = (id) => gql`
-{
-    product (id: "${id}" ) {
-        id
-        name
-        inStock
-        gallery
-        description
-        category
-        attributes {
-          id
-          name
-          type
-          items {
-            displayValue
-            value
-            id
-          }
-        }
-        prices {
-          currency {
-            label
-            symbol
-          }
-          amount
-        }
-        brand
-    }
-}`;
+const Boxes = styled.div`
+    display: flex;
+    width: 290px;
+    margin-bottom: 30px;
+`;
+const Image = styled.img`
+    display: block;
+    width: 610px;
+    height: 510px;
+    margin: 0 50px;
+`;
+const ImgItem = styled.a`
+    display: flex;
+    width: 79px;
+    height: 80px;
+    background-image: url(${props => props.src});
+    background-size: cover;
+    margin-bottom: 20px;
+`;
